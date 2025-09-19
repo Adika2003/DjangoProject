d124600ed611fc5ab2a86961d152b155881058b4
Adika2003 <adikasultana88m@gmail.com>
2025-09-19 18:42:39 +0600

    six commit

--- uapclub/events/views.py
+++ uapclub/events/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Event
+from .forms import EventForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.filter(published=True).order_by('start_time')
+    return render(request, 'events/event_list.html', {'events': events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'events/event_detail.html', {'event': event})
+
+@login_required
+def event_create(request):
+    if not request.user.is_staff:
+        messages.error(request, 'Only staff can create events.')
+        return redirect('events:event_list')
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            ev = form.save(commit=False)
+            ev.created_by = request.user
+            ev.save()
+            messages.success(request, 'Event created.')
+            return redirect('events:event_detail', pk=ev.pk)
+    else:
+        form = EventForm()
+    return render(request, 'events/event_form.html', {'form': form})
+
