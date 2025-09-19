22a9218514172c26e05efd28935e28f6ae4bd085
Adika2003 <adikasultana88m@gmail.com>
2025-09-19 18:44:11 +0600

    Eleven commit

--- uapclub/events/models.py
+++ uapclub/events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from clubs.models import Club
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='events')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
