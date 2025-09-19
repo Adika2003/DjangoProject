2d0acd242724fecdcc7aea935ef30ae8d0e744ab
Adika2003 <adikasultana88m@gmail.com>
2025-09-19 18:22:29 +0600

    Third commit

--- uapclub/uapclub/urls.py
+++ uapclub/uapclub/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+'from django.contrib import admin'
+'from django.urls import path'
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('clubs.urls')),      # home, clubs
+    path('events/', include('events.urls')),
+    path('blogs/', include('blogs.urls')),
+    path('contact/', include('contact.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
