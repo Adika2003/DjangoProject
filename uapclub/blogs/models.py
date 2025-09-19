fe4c664ff0a3189e6f708998b77bb0293be19ae8
Adika2003 <adikasultana88m@gmail.com>
2025-09-19 20:10:21 +0600

    22 commit

--- uapclub/blogs/models.py
+++ uapclub/blogs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='blogs/images/', blank=True, null=True)
+    published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)[:200]
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return self.title
+
