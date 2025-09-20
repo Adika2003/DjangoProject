73d0f9170b34ba757ffb704314aec6a9b633e608
Adika2003 <adikasultana88m@gmail.com>
2025-09-20 10:36:31 +0600

    2 error fix,26 commit

--- uapclub/events/admin.py
+++ uapclub/events/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Club
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title','club','start_time','published')
-    list_filter = ('club','published')
-    search_fields = ('title','description')
+    list_display = ('title', 'club', 'start_time', 'published')
+    list_filter = ('club', 'published')
+    search_fields = ('title', 'description')
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
